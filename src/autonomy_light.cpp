be48417099df3df86a95d9ddfef60e7b6577b3a0
Sanghyun Ryoo <sanghyunryoo@postech.ac.kr>
2026-07-14 21:51:23 +0900

    feat(build): open build to foxy

--- src/autonomy_light.cpp
+++ src/autonomy_light.cpp
@@ -33,7 +33,11 @@
 #include <tf2/LinearMath/Matrix3x3.h>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2/LinearMath/Vector3.h>
+#if __has_include(<tf2_geometry_msgs/tf2_geometry_msgs.hpp>)
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#else
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#endif
 #include <tf2_ros/static_transform_broadcaster.h>
 #include <tf2_ros/transform_broadcaster.h>
 
